95e09bccb4f2c0612e761b9dd86b7f60a677f55e
jcallec7 <jcallec7@hotmail.com>
2019-05-23 16:11:19 -0500

    correcion calculadora

--- Calculadora/calculadora.js
+++ Calculadora/calculadora.js
@@ -4,68 +4,55 @@ var n2 = "";
 var nuevoNum = "blank";
 var opvalue = "";
 
-function pantalla(pantallanumber) 
-{
-document.calculator.answer.value = pantallanumber;
+function pantalla(pantallanumber) {
+   document.calculator.answer.value = pantallanumber;
 }
 
-function borrar() 
-{
-data = 0;
-document.calculator.mem.value = "";
+function borrar() {
+   data = 0;
+   document.calculator.mem.value = "";
 }
 
-function borrado() 
-{
-n1 = "";
-n2 = "";
-nuevoNum = "blank";
-pantalla("");
+function borrado() {
+   n1 = "";
+   n2 = "";
+   nuevoNum = "blank";
+   pantalla("");
 }
 
-function borradoMemoria()
-{
-n2 = "";
-nuevoNum = "yes";
-pantalla("");
+function borradoMemoria() {
+   n2 = "";
+   nuevoNum = "yes";
+   pantalla("");
 }
 
-function setNum(answer) 
-{
-   if(answer == ".")
-   {
+function setNum(answer) {
+   if (answer == ".") {
       Number = document.calculator.answer.value;
-      if(Number.indexOf(".") != -1) 
-      {
-      answer = "";
+      if (Number.indexOf(".") != -1) {
+         answer = "";
       }
    }
-   if(nuevoNum == "yes") 
-   {
+   if (nuevoNum == "yes") {
       n2 += answer;
       pantalla(n2);
    }
-   else 
-   {
-      if(nuevoNum == "blank")
-      {
+   else {
+      if (nuevoNum == "blank") {
          n1 = answer;
          n2 = "";
          nuevoNum = "no";
       }
-      else 
-      {
+      else {
          n1 += answer;
       }
       pantalla(n1);
    }
 }
 
-function suma(x) 
-{
-   if(x == 1) igual();
-   if(n2 != "")
-   {
+function suma(x) {
+   if (x == 1) igual();
+   if (n2 != "") {
       n1 = parseFloat(n1) + parseFloat(n2);
    }
    nuevoNum = "yes";
@@ -73,84 +60,72 @@ function suma(x)
    pantalla(n1);
 }
 
-function resta(x) 
-{
-   if(x == 1) igual();
-   if(n2 != "") 
-   {
-       n1 = parseFloat(n1) - parseFloat(n2);
+function resta(x) {
+   if (x == 1) igual();
+   if (n2 != "") {
+      n1 = parseFloat(n1) - parseFloat(n2);
    }
    nuevoNum = "yes";
    opvalue = '-';
    pantalla(n1);
 }
 
-function multi(x) 
-{
-   if(x == 1) igual();
-   if(n2 != "")
-   {
-     n1 = parseFloat(n1) * parseFloat(n2);
+function multi(x) {
+   if (x == 1) igual();
+   if (n2 != "") {
+      n1 = parseFloat(n1) * parseFloat(n2);
    }
    nuevoNum = "yes";
    opvalue = '*';
    pantalla(n1);
 }
 
-function division(x) 
-{
-   if(x == 1) igual();
-   if(n2 != "")
-   {
+function division(x) {
+   if (x == 1) igual();
+   if (n2 != "") {
       n1 = parseFloat(n1) / parseFloat(n2);
    }
    nuevoNum = "yes";
    opvalue = '/';
    pantalla(n1);
 }
-function raiz()
-{
+function raiz() {
    n1 = Math.sqrt(n1);
    nuevoNum = "blank";
    pantalla(n1);
 }
-function porcentaje() 
-{
-   if(nuevoNum != "blank") 
-   {
+function porcentaje() {
+   if (nuevoNum != "blank") {
       n2 *= .01;
       nuevoNum = "blank";
       pantalla(n2);
    }
 }
-function cuadrado()
-{
-   n1 = Math.pow(n1,2);
-   nuevoNum="blank";
-   pantalla(n1);
-}
-function divUno() 
-{
-   n1 = 1/n1;
+function cuadrado() {
+   n1 = Math.pow(n1, 2);
    nuevoNum = "blank";
    pantalla(n1);
 }
-function negativo() 
-{
+function divUno() {
+   n1 = 1 / n1;
+   nuevoNum = "blank";
+   pantalla(n1);
+}
+function negativo() {
    n1 = parseFloat(-n1);
    nuevoNum = "no";
    pantalla(n1);
 }
 function igual() {
-if(opvalue == '²') cuadrado(0);
-if(opvalue == '+') suma(0);
-if(opvalue == '-') resta(0);
-if(opvalue == '*') multi(0);
-if(opvalue == '/') division(0);
-if(opvalue == '√') raiz(0);
-if(opvalue == '%') porcentaje(0);
-if(opvalue == '1/x') divUno(0);
-if(opvalue == '+/-') negativo(0);
-n2 = "";
-opvalue = "";
+   if (opvalue == '²') cuadrado(0);
+   if (opvalue == '+') suma(0);
+   if (opvalue == '-') resta(0);
+   if (opvalue == '*') multi(0);
+   if (opvalue == '/') division(0);
+   if (opvalue == '√') raiz(0);
+   if (opvalue == '%') porcentaje(0);
+   if (opvalue == '1/x') divUno(0);
+   if (opvalue == '+/-') negativo(0);
+   n2 = "";
+   opvalue = "";
 }
